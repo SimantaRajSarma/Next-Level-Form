0875300fc75c29ecf45a35b3f6af8e6d9d838a40
SimantaRajSarma <simantarajsarma@gmail.com>
2024-03-16 03:13:41 +0530

    done bhai

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,42 @@
-// App.tsx
-import Form from './components/form/Form'
+import React from 'react';
+import SageForm, { SageFormComponent } from './components/form/Form';
 
 const App: React.FC = () => {
-
-  interface InputFieldConfig {
-    component: 'input';
-    placeholder: string;
-    tag: string;
-    type: string;
-}
-
-interface TextAreaFieldConfig {
-    component: 'textarea';
-    placeholder: string;
-    tag: string;
-    type?: never;
-}
-
-type FieldConfig = InputFieldConfig | TextAreaFieldConfig;
-
-const config: FieldConfig[] = [
+  const config: SageFormComponent[] = [
     {
-        component: 'input',
-        placeholder: 'Username',
-        tag: 'username',
-        type: 'text',
+      component: 'input',
+      placeholder: 'Username',
+      tag: 'username',
+      type: 'text',
     },
     {
-        component: 'input',
-        placeholder: 'Password',
-        tag: 'password',
-        type: 'password',
+      component: 'input',
+      placeholder: 'Password',
+      tag: 'password',
+      type: 'password',
     },
     {
-        component: 'textarea',
-        placeholder: 'Address',
-        tag: 'address',
+      component: 'text-area',
+      placeholder: 'Address',
+      tag: 'address',
     },
     {
-      component: "input",
-      placeholder: "gender",
-      tag: "gender",
-      type: "text",
+      component: 'input',
+      placeholder: 'Gender',
+      tag: 'gender',
+      type: 'text',
     },
-
-];
-
+  ];
 
   const sendFormData = (formData: any) => {
     console.log(formData);
   };
-
+  
   return (
-    <div>
-      <h1 className='titleStyle font-bold'>Ye Form Nehi hai bhai :)</h1>
-      <Form config={config} onSubmit={sendFormData} />
-    </div>
+    <>
+      <h1 className='titleStyle font-bold'>Form Component</h1>
+      <SageForm component={config} onSubmit={sendFormData} />
+   </>
   );
 };
 
--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,24 +1,74 @@
-// DynamicForm.tsx
-import { useState } from 'react';
-import InputField from './InputField';
-import TextAreaField from './TextAreaField';
+import React, { FC, useState } from "react";
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
 
-
-
-interface FieldConfig {
+type SageFormComponentBasic = {
   component: string;
-  placeholder: string;
   tag: string;
-  type?: string;
-}
+};
 
-interface DynamicFormProps {
-  config: FieldConfig[];
-  onSubmit: (value: any) => void;
-}
+type SageFormInput = SageFormComponentBasic & {
+  component: "input";
+  placeholder: string;
+  type: string;
+  min?: number;
+  max?: number;
+};
 
-const DynamicForm: React.FC<DynamicFormProps> = ({ config, onSubmit }) => {
+type SageFormTextArea = SageFormComponentBasic & {
+  component: "text-area";
+  placeholder: string;
+};
+
+type SageFormRadio = {
+  value: string;
+  label: string;
+};
+
+type SageFormRadioGroup = SageFormComponentBasic & {
+  component: "radio-group";
+  radioComponents: SageFormRadio[];
+};
+
+export type SageFormComponent =
+  | SageFormInput
+  | SageFormTextArea
+  | SageFormRadioGroup;
+
+type SageFormParams = {
+  component: SageFormComponent[];
+  onSubmit: (formData: any) => void;
+};
+
+const SageForm: FC<SageFormParams> = ({ component, onSubmit }) => {
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const formDataObject: Record<string, any> = {};
+    for (let [key, value] of formData.entries()) {
+      formDataObject[key] = value;
+    }
+    onSubmit(formDataObject);
+  };
+
+  return (
+    <div className='container'>
+    <form onSubmit={handleSubmit}>
+      {component.map((value, index) => {
+        return <Component key={index} component={value} />;
+      })}
+       <div className='text-center text-lg px-4 py-2'>
+      <Button type="submit">Submit</Button>
+      </div>
+    </form>
+    </div>
+  );
+};
+
+const Component = ({ component }: { component: SageFormComponent; }) => {
+
   const [formData, setFormData] = useState<{ [key: string]: string }>({});
 
   const handleChange = (value: string, tag: string) => {
@@ -28,51 +78,55 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ config, onSubmit }) => {
     });
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    const formattedData: { [key: string]: string } = {};
-    config.forEach(field => {
-      formattedData[field.tag] = formData[field.tag];
-    });
-    onSubmit(formattedData);
-  };
-  
+  switch (component.component) {
+    case "input":
+      return (
+        <div style={{ marginBottom: '10px' }}>
+        <SageFormInput
+          config={component}
+          onChange={(value) => handleChange(value, component.tag)}
+        />
+        </div>
+      );
+      case "text-area":
+        return (
+        <div style={{ marginBottom: '10px' }}> 
+        <SageFormTextArea 
+        config={component} 
+        onChange={(value) => handleChange(value, component.tag)}
+         />
+         </div>
+        );
+    default:
+      return <h1>Nothing to render</h1>;
+  }
+};
+
+const SageFormInput = ({
+  config,
+  onChange,
+}: {
+  config: SageFormInput;
+  onChange: (value: string) => void;
+}) => {
   return (
-    <div className='container'>
-    <form onSubmit={handleSubmit}>
-      {config.map((field, index) => {
-        if (field.component === 'input') {
-          return (
-            <div key={index} style={{ marginBottom: '10px' }}>
-            <InputField
-              key={index}
-              placeholder={field.placeholder}
-              value={formData[field.tag] || ''}
-              onChange={(value) => handleChange(value, field.tag)}
-              type={field.type}
-            />
-            </div>
-          );
-        } else if (field.component === 'textarea') {
-          return (
-            <div key={index} style={{ marginBottom: '10px' }}>
-            <TextAreaField
-              key={index}
-              placeholder={field.placeholder}
-              value={formData[field.tag] || ''}
-              onChange={(value) => handleChange(value, field.tag)}
-            />
-            </div>
-          );
-        }
-        return null;
-      })}
-      <div className='text-center text-lg px-4 py-2'>
-      <Button type="submit">Submit</Button>
-      </div>
-    </form>
-    </div>
+    <Input
+      {...config}
+      name={config.tag}
+      onChange={(e) => onChange(e.target.value)}
+    />
   );
 };
 
-export default DynamicForm;
+const SageFormTextArea = ({ config, onChange }: { config: SageFormTextArea; onChange: (value: string) => void }) => {
+  return (
+    <Textarea
+      name={config.tag}
+      placeholder={config.placeholder}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
+};
+
+
+export default SageForm;
